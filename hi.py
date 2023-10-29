d1dfe672877d6b30a310402f9028c7d8a90624e6
Karem <bendrisskarem@gmail.com>
2023-10-29 22:32:24 +0100

    ajout alo

--- hi.py
+++ hi.py
@@ -1,29 +1,46 @@
 import os
 import datetime
 
+
+## function time now
+def timez():
+    s = datetime.datetime.now()
+    p = s.strftime("d%H%M")
+    return(p)
+ee = "Bureau/lll"
+##fonction concatination path
+def pathz():
+    s1=timez()
+
+    z=str(ee)+str(s1)+".txt"
+    return(str(z))
+
+## Fonction path
+def path(x):
+    path=str(x)
+    return(path)
 i=0
 # Chemin d'accès du dossier à créer
-"""new_directory_path = r'Bureau\lll'
+new_directory_path = r'Bureau/lll'
+new_directory_path1 = path(ee)
+
+
+
 def concatin(a,b):
     z=str(a)+str(b)
     return(z)
 
-def path(x):
-    path="r"+str(x)
+
     
 # Vérifier si le dossier existe déjà
-if not os.path.exists(new_directory_path):
+if not os.path.exists(ee):
     # Créer le dossier
-    os.makedirs(new_directory_path)
+    os.makedirs(ee)
     print(f'Le dossier {new_directory_path} a été créé avec succès.')
 else:
-    heure_actuelle = str(datetime.datetime.now().time())
 
-    chemin = r'Bureau\lll\ll.txt'
-    with open(chemin, mode='w') as fichier:
+
+    chemin = r'Bureau/lll/ll.txt'
+    with open(pathz(), mode='w') as fichier:
         pass
-    print(f'Le dossier {new_directory_path} existe déjà, il est recréé')"""
-
-s = datetime.datetime.now()
-p=s.strftime("%Y-%m-%d %H:%M:%S")
-print(p)
+    print(f'Le dossier {new_directory_path1} existe déjà, il est recréé')
